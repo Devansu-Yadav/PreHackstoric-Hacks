db8d08db76bc278980609a0252a64e85e19d4e61
Devansu <devansuyadav@gmail.com>
2021-09-10 23:03:50 +0530

    Update: Started with User Auth using Auth0

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Auth0ProviderWithHistory>
+        <App />
+        </ Auth0ProviderWithHistory>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
