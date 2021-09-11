c73c413e74e3453d5d382ee920b668c7a1d092a0
Devansu <devansuyadav@gmail.com>
2021-09-11 15:25:03 +0530

    Update: Completed User Authentication

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import NavBar from "./components/navbar";
+import Loading from "./components/loading";
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react"
+
 function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+  
   return (
-    <></>
+    <div id="app">
+      <NavBar />
+    </div>
   );
 }
 
